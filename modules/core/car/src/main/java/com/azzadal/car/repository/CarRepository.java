0a4187c4b153d045e9d7f6514b594a979b3468ce
Azzadal <kvasov2803@gmail.com>
2024-01-22 13:04:08 +0700

    scan components

--- modules/core/car/src/main/java/com/azzadal/car/repository/CarRepository.java
+++ modules/core/car/src/main/java/com/azzadal/car/repository/CarRepository.java
@@ -1,7 +1,9 @@
 package com.azzadal.car.repository;
 
 import com.azzadal.car.model.Car;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CarRepository extends CrudRepository<Car, Long> {
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
 }
